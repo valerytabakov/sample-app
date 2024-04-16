fb6ad27f3817ddc75ca41fe8ce166c4bc74f4d31
valerytabakov <valerytabakov@gmail.com>
2024-04-16 11:43:05 +0300

    new commit

--- sample_app.py
+++ sample_app.py
@@ -7,8 +7,8 @@ sample = Flask(__name__)
 @sample.route('/')
 def main():
     # return "You are calling me from " + request.remote_addr + "\n"
-    # return render_template("index.html")
-     return 'Hello World from Flask Web Server!***'
+    return render_template("index.html")
+     # return 'Hello World from Flask Web Server!***'
 
 if __name__=="__main__":
     sample.run(host='0.0.0.0', port=8080)
