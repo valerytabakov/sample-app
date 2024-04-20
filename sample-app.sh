f53d6a00e9d2eb205d16cb090eae8685a80dff9b
valerytabakov <valerytabakov@gmail.com>
2024-04-20 18:07:39 +0300

    new commit

--- sample-app.sh
+++ sample-app.sh
@@ -8,7 +8,7 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM pyton" > tempdir/Dockerfile
+echo "FROM python" > tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 
 echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
